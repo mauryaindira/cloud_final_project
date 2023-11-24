0c5d6ce90b50453d56658782c7957f29aa245892
indira maurya <maurya.10@iitj.ac.in>
2023-11-25 04:43:04 +0530

    submission

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # cloud_final_project
+
+Step 1: Create an flask application(Web_app.py) and running the file on web url.
+
+Step 2: create a Requirement file (req.text)
+
+    flask
+    sqlite3
+    google-cloud-pubsub
+
+
+Step 3: Create a Dockerfile FROM python:3.6 WORKDIR /app COPY . . RUN pip install -r req.txt EXPOSE 80 CMD ["python", "cloud_asign1.py"]
+
+Command: sudo docker build -t flask_app .
+
+ # Docker file will pull the python: 3.6
+ # Now will it will create directory with the 'app' name.
+ # It will copy the files from root directory to docker file
+ # Now it will install the requirement file 
+ # It expose the app on port 80
+ # CMD run python file cloud_assign1.py
+Step 4: sudo docker run -p 80:80 flask_app:latest
+
+    This command will run the image on port 80.
+
+
+    
+Author : Indira Maurya
+
+Roll Number: M21AIE228
---
+++ final_project/Web_app.py
@@ -0,0 +1,96 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+import sqlite3
+#from requests import request
+from flask import Flask,render_template, redirect, request #, send_mail
+import os
+from google.cloud import pubsub_v1
+
+#Setting path pub/sub
+os.environ["GCLOUD_PROJECT"] = "teak-formula-362813"
+cred_path = "./resources//application_default_credentials.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = cred_path
+ 
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
+app = Flask(__name__)
+ 
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
+@app.route('/', methods=['POST'])
+
+def order_post():
+    # if request.method == 'POST':
+    name = request.form['name']
+    email = request.form['email']
+    selected_items = request.form.getlist('order')
+
+    # publisher code
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path("teak-formula-362813", "M21AIE228_token1")
+    # Data must be a bytestring
+    data = "Hi " + name + ", your orders are: " + str(selected_items[0])
+    
+    data = data.encode("utf-8")
+    # When you publish a message, the client returns a future.
+    future = publisher.publish(topic_path, data)
+    print(future.result())
+
+    # Send order confirmation email
+    confirmation_email = f"Thank you for your order, {name}! You have selected the following items: {selected_items}"
+    #send_mail(name, "Order Confirmation", confirmation_email)
+
+    # Save order to database
+    # Connect to the database
+    conn = sqlite3.connect('mysql_database.db')
+    cursor = conn.cursor()
+
+    #Create table for order
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS orders (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            email TEXT
+        )
+    ''')
+    # Create a new order record
+    cursor.execute("INSERT INTO orders (name, email) VALUES (?, ?)", (name, email))
+    order_id = cursor.lastrowid
+    
+    # Create table for order_item
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS order_items (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        order_id INTEGER,
+        item TEXT,
+        FOREIGN KEY (order_id) REFERENCES orders (id)
+    )
+''')
+
+    # Insert each selected item into the order_items table
+    for item in selected_items:
+        cursor.execute("INSERT INTO order_items (order_id, item) VALUES (?, ?)", (order_id, item))
+
+    # Commit the changes to the database
+    conn.commit()
+    conn.close()
+
+    # Redirect to thank you page
+    return render_template('thankyou.html')
+
+
+
+@app.route('/', methods=['GET'])
+
+def order_get():
+    return render_template('order.html')
+    
+ 
+if __name__== "__main__":
+    app.run(debug= True)
+
+
+
+
+
